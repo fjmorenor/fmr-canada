805a6718bf4824dee578a0eb1104f1d5b4e527c7
fjmorenor <64865231+fjmorenor@users.noreply.github.com>
2020-07-24 18:07:26 +0200

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "nat" {
   ami                         = "ami-0346ee471bb2c892a"
-  availability_zone           = "ca-central-1"
+  availability_zone           = "ca-central-1a"
   instance_type               = "t2.micro"
   key_name                    = var.aws_key_name
   vpc_security_group_ids      = [aws_security_group.nat.id]
-  subnet_id                   = aws_subnet.ca-central-1b-public.id
+  subnet_id                   = aws_subnet.ca-central-1a-public.id
   associate_public_ip_address = true
   source_dest_check           = false
 
@@ -15,11 +15,11 @@ resource "aws_instance" "nat" {
 
 resource "aws_instance" "web-1" {
   ami                         = var.amis[var.aws_region]
-  availability_zone           = "ca-central-1"
+  availability_zone           = "ca-central-1a"
   instance_type               = "t2.micro"
   key_name                    = var.aws_key_name
   vpc_security_group_ids      = [aws_security_group.web.id]
-  subnet_id                   = aws_subnet.ca-central-1b-public.id
+  subnet_id                   = aws_subnet.ca-central-1a-public.id
   associate_public_ip_address = true
   source_dest_check           = false
 
@@ -34,7 +34,7 @@ resource "aws_instance" "db-1" {
   instance_type          = "t2.micro"
   key_name               = var.aws_key_name
   vpc_security_group_ids = [aws_security_group.db.id]
-  subnet_id              = aws_subnet.ca-central-1-private.id
+  subnet_id              = aws_subnet.ca-central-1a-private.id
   associate_public_ip_address = true
   source_dest_check      = false
 
